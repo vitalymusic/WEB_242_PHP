636ddf18da988ae525be562ef114d7f1b859b184
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-25 21:32:24 +0200

    php 4

---
+++ features.php
@@ -0,0 +1,10 @@
+<?php include "head.php"?>
+<?php include_once "navbar.php"?>
+
+<div class="container">
+    <h1 class="text-center">Features</h1>
+    <p>This is content text about features</p>
+    <?php include "feat_blocks.php" ?>
+</div>
+
+<?php include "foot.php"?>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">My php site</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +10,7 @@
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="features.php">Features</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
