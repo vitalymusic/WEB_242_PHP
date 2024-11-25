371a7852c9b6fcd2b9e0190256b1c40775fea88e
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-25 21:40:26 +0200

    php4

--- navbar.php
+++ navbar.php
@@ -15,9 +15,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
     </div>
   </div>
