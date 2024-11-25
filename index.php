a0ec1065c4ee60e193396a1c185815b8ce301fb2
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-25 19:16:21 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    phpinfo();
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php ?></title>
+</head>
+<body>
+    <h1><?php ?></h1>
+    <p><?php ?></p>
+</body>
+</html>
