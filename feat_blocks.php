9a06d195811d1ab4b218e9f0b5d4ecac9d223dfa
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-28 19:07:23 +0200

    php

--- feat_blocks.php
+++ feat_blocks.php
@@ -1,12 +1,38 @@
+<?php 
+
+var_dump($data);
+
+
+$data["features"];
+$out = "";
+
+
+for($i = 0;$i<sizeof($data["features"]);$i++){
+$out .= '
+    <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+';
+
+}
+
+?>
+
+
 <h2 class="mb-3">Our features:</h2>
 <div class="cards d-flex justify-content-center gap-3">
     <div class="card" style="width: 18rem;">
-    <img src="" class="card-img-top" alt="">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
     <div class="card" style="width: 18rem;">
     <img src="" class="card-img-top" alt="">
--- settings.php
+++ settings.php
@@ -1,6 +1,47 @@
 <?php
     $title = "My first web page using PHP";
     
+    
+
+    $data = [];
 
 
+    $data["features"] = [
+        [
+            "name"=>"WEB Sites",
+            "text"=>"We are specialists of creating web",
+            "img"=>"",
+            "href"=>"./feature1.php"
+        ],
+        [
+            "name"=>"Computer repair",
+            "text"=>"We are ready to repair your PC",
+            "img"=>"",
+            "href"=>"./feature2.php"
+        ],
+        [
+            "name"=>"CMS integration",
+            "text"=>"We can integrate CMS in Your site",
+            "img"=>"",
+            "href"=>"./feature3.php"
+        ],
+    ];
+
+
+    $data["users"] = [
+        [
+            "userid"=>1,
+            "email"=>"admin@website.com",
+            "name"=>"Alex"
+
+        ],
+        [
+            "userid"=>2,
+            "email"=>"user@website.com",
+            "name"=>"Andrey"
+
+        ],
+    ];
+
+        // var_dump($data);
 ?>
