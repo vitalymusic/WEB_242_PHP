52e002aba1c4dbc30d73d9e6531ca61204b259b4
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-12-02 21:26:48 +0200

    db

--- db.php
+++ db.php
@@ -18,20 +18,32 @@
     if($result->num_rows>0){
         
         while($row = $result->fetch_assoc()){
+            $data[] = $row; //Для вывода данных в JSON
+
             echo "<h1>{$row["post_name"]}</h1>";
             echo "<p>{$row["content"]}</p>";
             echo "<input type='text' value='{$row["date"]}'>";
         }
 
         // echo json_encode($data);
-
-
         // print_r($data);
 
     }
 
-    
 
+    function delPost($id){
+        global $link;
 
+        $sql = "DELETE FROM `posts` WHERE id={$id}";
+        $result = $link->query($sql);
+        if($result){
+            echo "Post deleted";
+        }else{
+            echo $result->error;
+        }
+
+    }
+
+    delPost(4);
 
 ?>
