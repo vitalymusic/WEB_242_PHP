b7b080d8b6bbfc81ee36ba94cc53c70020a1689d
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-12-02 21:15:20 +0200

    db

--- db.php
+++ db.php
@@ -6,10 +6,31 @@
     exit($link->connect_error);
  }
 
-
- 
     echo "Connected";
 
+
+    $sql = "SELECT * from posts";
+
+    $result = $link->query($sql);
+
+    $data = [];
+
+    if($result->num_rows>0){
+        
+        while($row = $result->fetch_assoc()){
+            echo "<h1>{$row["post_name"]}</h1>";
+            echo "<p>{$row["content"]}</p>";
+            echo "<input type='text' value='{$row["date"]}'>";
+        }
+
+        // echo json_encode($data);
+
+
+        // print_r($data);
+
+    }
+
+    
 
 
 
